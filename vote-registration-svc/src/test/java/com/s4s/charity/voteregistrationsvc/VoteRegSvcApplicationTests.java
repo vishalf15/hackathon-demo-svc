f80709a2d36ed435f42b1456236237e53c08b606
suryaatul1 <116798411+suryaatul1@users.noreply.github.com>
2023-09-22 09:18:12 +0530

    Add files via upload

---
+++ vote-registration-svc/src/test/java/com/s4s/charity/voteregistrationsvc/VoteRegSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s4s.charity.voteregistrationsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteRegSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
